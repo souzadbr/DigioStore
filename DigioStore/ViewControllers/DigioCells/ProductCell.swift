c72eb7b9acd2e5d794b4f20b34ae20724f1f83fc
Débora Rodrigues <81527964+souzadbr@users.noreply.github.com>
2024-06-28 20:40:05 -0300

    criar celula personalizada para CollectionView

---
+++ DigioStore/ViewControllers/DigioCells/ProductCell.swift
@@ -0,0 +1,50 @@
+//
+//  ProductCell.swift
+//  DigioStore
+//
+//  Created by Debora Rodrigues  on 28/06/24.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    static let reuseIdentifier = "ProductCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with url: URL) {
+        loadImage(from: url, into: imageView)
+    }
+    
+    private func loadImage(from url: URL, into imageView: UIImageView) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async {
+                imageView.image = UIImage(data: data)
+            }
+        }
+        task.resume()
+    }
+}
+
