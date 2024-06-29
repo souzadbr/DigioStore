3e9ac4c9dfbfb96569308b813a084f298e5fe5dc
Débora Rodrigues <81527964+souzadbr@users.noreply.github.com>
2024-06-29 20:31:07 -0300

    refatorar Detalhe do produto

--- DigioStore/ViewControllers/ProductDetailViewController.swift
+++ DigioStore/ViewControllers/ProductDetailViewController.swift
@@ -7,34 +7,25 @@
 import UIKit
 
 class ProductDetailViewController: UIViewController {
-    
     var viewModel: ProductDetailViewModel? {
         didSet {
             configureView()
         }
     }
-    
+
     private let productDetailView = ProductDetailView()
-    
+
     override func loadView() {
         view = productDetailView
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationItem.title = "Produto"
-        
-        if #available(iOS 13.0, *) {
-            productDetailView.enableProductSwitch.addTarget(self, action: #selector(enableProductSwitchChanged), for: .valueChanged)
-        } else {
-            // Fallback para versões anteriores
-            let alert = UIAlertController(title: "Atualização necessária", message: "Por favor, atualize seu iOS para a versão 13.0 ou superior para usar essa funcionalidade.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
-        }
+        productDetailView.enableProductSwitch.addTarget(self, action: #selector(enableProductSwitchChanged), for: .valueChanged)
     }
-    
+
     private func configureView() {
         guard let viewModel = viewModel else { return }
         productDetailView.productNameLabel.text = viewModel.productName
@@ -43,8 +34,7 @@ class ProductDetailViewController: UIViewController {
             self?.productDetailView.productImageView.image = image
         }
     }
-    
-    @available(iOS 13.0, *)
+
     @objc private func enableProductSwitchChanged(_ sender: UISwitch) {
         if sender.isOn {
             showBottomSheet()
@@ -52,10 +42,9 @@ class ProductDetailViewController: UIViewController {
             showDisableAlert()
         }
     }
-    
+
     private func showBottomSheet() {
         let bottomSheetVC = BottomSheetViewController()
-        
         if #available(iOS 15.0, *) {
             bottomSheetVC.modalPresentationStyle = .pageSheet
             if let sheet = bottomSheetVC.sheetPresentationController {
@@ -64,16 +53,18 @@ class ProductDetailViewController: UIViewController {
         } else {
             bottomSheetVC.modalPresentationStyle = .overFullScreen
         }
-        
         present(bottomSheetVC, animated: true, completion: nil)
     }
-    
-    @available(iOS 13.0, *)
+
     private func showDisableAlert() {
         let alert = UIAlertController(title: nil, message: "Este produto está sendo desabilitado e não contará na próxima fatura do seu cartão Digio.", preferredStyle: .alert)
         
-        let imageView = UIImageView(frame: CGRect(x: 220, y: 10, width: 40, height: 40))
-        alert.view.addSubview(imageView)
+        if #available(iOS 13.0, *) {
+            let imageView = UIImageView(frame: CGRect(x: 220, y: 10, width: 40, height: 40))
+            imageView.image = UIImage(systemName: "exclamationmark.triangle.fill")
+            imageView.tintColor = .red
+            alert.view.addSubview(imageView)
+        }
         
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
--- DigioStore/ViewModel/ProductDetailViewModel.swift
+++ DigioStore/ViewModel/ProductDetailViewModel.swift
@@ -8,42 +8,39 @@ import Foundation
 import UIKit
 
 class ProductDetailViewModel {
-    
-    private var product: Products
-    
-    var productName: String {
-        return product.name
-    }
-    
-    var productDescription: String {
-        return product.description
-    }
-    
-    var productImageURL: URL? {
-        return URL(string: product.imageURL)
-    }
-    
+    let product: Products
+
     init(product: Products) {
         self.product = product
     }
-    
+
+    var productName: String {
+        return product.name
+    }
+
+    var productDescription: String {
+        return product.description
+    }
+
     func loadImage(completion: @escaping (UIImage?) -> Void) {
-        guard let url = productImageURL else {
+        guard let url = URL(string: product.imageURL) else {
             completion(nil)
             return
         }
-        
+
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 print("Error loading image: \(error)")
                 completion(nil)
                 return
             }
-            guard let data = data, let image = UIImage(data: data) else {
+            guard let data = data else {
+                print("No data found")
                 completion(nil)
                 return
             }
             DispatchQueue.main.async {
+                let image = UIImage(data: data)
                 completion(image)
             }
         }
